99bc920f86d19d49f4160bba29ff42507ac68419
JasurbekJapan <olloyorovjasurbek2005@gmail.com>
2025-09-26 16:22:19 +0500

    Refactor repository interfaces and update ReportService
    Removed `IAuthorRepository` and moved `GetBorrowedBooksAsync`
    to the newly introduced `IReportRepository`. Updated the
    `ReportService` class to implement `IReportService` and added
    a placeholder `GenerateReportAsync` method. Corrected namespace
    usages in `ReportService` to include necessary dependencies.

--- Libray_Managment_System/Libray_Managment_System/Services/ReportService/IReportRepository.cs
+++ Libray_Managment_System/Libray_Managment_System/Services/ReportService/IReportRepository.cs
@@ -1,6 +1,6 @@
 ﻿namespace Libray_Managment_System.Services.ReportService;
 
-public interface IAuthorRepository
+public interface IReportRepository
 {
     Task<IEnumerable<Models.Book>> GetBorrowedBooksAsync(DateTime from, DateTime to);
 }
--- Libray_Managment_System/Libray_Managment_System/Services/ReportService/ReportService.cs
+++ Libray_Managment_System/Libray_Managment_System/Services/ReportService/ReportService.cs
@@ -1,6 +1,12 @@
-﻿namespace Libray_Managment_System.Services.ReportService;
+﻿using Libray_Managment_System.DtoModels;
+using Libray_Managment_System.Enum;
 
-public class ReportService
+namespace Libray_Managment_System.Services.ReportService;
+
+public class ReportService : IReportService
 {
-    
+    public Task<ReportDTO> GenerateReportAsync(ReportType type, DateTime? from = null, DateTime? to = null)
+    {
+        throw new NotImplementedException();
+    }
 }
