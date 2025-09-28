87ac69f3c7c81e7ffba96c1a15c5a55d41f0297b
xusanov10 <ahmadjonxusanovv>
2025-09-28 20:08:01 +0500

    Add AuthController for user registration and login
    Introduces the `AuthController` class in the `Libray_Managment_System.Controllers` namespace. This controller handles HTTP requests for user registration and login at the routes `api/auth/register` and `api/auth/login`, respectively. It utilizes the `IAuthService` to manage the business logic and returns appropriate responses based on the outcome of the operations.

---
+++ Libray_Managment_System/Libray_Managment_System/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Libray_Managment_System.DtoModels;
+using Libray_Managment_System.Services.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libray_Managment_System.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDTO dto)
+        {
+            var result = await _authService.RegisterUserAsync(dto);
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO dto)
+        {
+            var token = await _authService.LoginUserAsync(dto);
+            if (string.IsNullOrEmpty(token))
+                return Unauthorized("Invalid email or password");
+
+            return Ok(token);
+        }
+    }
+}
